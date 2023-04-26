2de53c1bba0b453c7957526c4ad5684eb824edf0
SonikeDev <119739711+SonikeDev@users.noreply.github.com>
2023-04-26 18:36:21 +0200

    Update script.js

--- Quill/teamquack/script.js
+++ Quill/teamquack/script.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCsUGjFG0mRf0CaX0mw-oCtSPftPkKrFNk",
-  authDomain: "quillchat-30e34.firebaseapp.com",
-  databaseURL: "https://quillchat-30e34-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "quillchat-30e34",
-  storageBucket: "quillchat-30e34.appspot.com",
-  messagingSenderId: "1035018230540",
-  appId: "1:1035018230540:web:981d388aec5164137b4729"
+  apiKey: "process.env.API_KEY",
+  authDomain: "process.env.AUTH_DOMAIN",
+  databaseURL: "process.env.DATABASE_URL",
+  projectId: "process.env.PROJECT_ID",
+  storageBucket: "process.env.STORAGE_BUCKET",
+  messagingSenderId: "process.env.MESSAGING_SENDER_ID",
+  appId: "process.env.APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
