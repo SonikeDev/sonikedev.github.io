bdd62737f5f4288ee6facb47463ce96309d5890c
SonikeDev <119739711+SonikeDev@users.noreply.github.com>
2023-05-03 11:34:52 +0200

    Add files via upload

---
+++ Quill/teamquack/script.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsUGjFG0mRf0CaX0mw-oCtSPftPkKrFNk",
+  authDomain: "quillchat-30e34.firebaseapp.com",
+  databaseURL:
+    "https://quillchat-30e34-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "quillchat-30e34",
+  storageBucket: "quillchat-30e34.appspot.com",
+  messagingSenderId: "1035018230540",
+  appId: "1:1035018230540:web:981d388aec5164137b4729",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
