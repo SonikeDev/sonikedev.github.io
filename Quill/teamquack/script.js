f436f43e85d9523ca96430c93f0e74ef21a024a8
SonikeDev <119739711+SonikeDev@users.noreply.github.com>
2024-06-06 11:19:33 +0200

    analytics

--- Quill/teamquack/script.js
+++ Quill/teamquack/script.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCsUGjFG0mRf0CaX0mw-oCtSPftPkKrFNk",
   authDomain: "quillchat-30e34.firebaseapp.com",
-  databaseURL:
-    "https://quillchat-30e34-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://quillchat-30e34-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "quillchat-30e34",
   storageBucket: "quillchat-30e34.appspot.com",
   messagingSenderId: "1035018230540",
   appId: "1:1035018230540:web:981d388aec5164137b4729",
+  measurementId: "G-K6MNME6S21"
 };
 
 firebase.initializeApp(firebaseConfig);
